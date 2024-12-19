fe2b5b52c4ceca93081fd7ab6057f296349f7baa
gaozhan <gaozhanwk@163.com>
2024-12-19 09:53:50 +0800

    添加 sdcv 搜索快捷键 Spc s w
    Signed-off-by: gaozhan <gaozhanwk@163.com>

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -2,4 +2,7 @@
 
 (global-set-key (kbd "<f12>") 'my/yank-image-from-win-clipboard-through-powershell)
 
+(map! :leader
+      :desc "sdcv search word" "s w" #'sdcv-search-pointer+)
+
 (provide 'init-keybinding)
